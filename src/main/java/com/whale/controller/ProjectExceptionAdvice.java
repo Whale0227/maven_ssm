fecbe5e2b6a162a9f30047f51e3e2c8056ee45b6
Whale <409134834@qq.com>
2024-07-12 11:38:52 +0800

    Revert "系统异常和业务异常处理并进行异常测试"
    This reverts commit 168b96ea2081e34813022052409486659f7ef2b1.

--- src/main/java/com/whale/controller/ProjectExceptionAdvice.java
+++ src/main/java/com/whale/controller/ProjectExceptionAdvice.java
@@ -1,37 +1,13 @@
 package com.whale.controller;
 
-import com.whale.exception.BusinessException;
-import com.whale.exception.SystemException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice   // 开启异常处理器
 public class ProjectExceptionAdvice {
-
-    @ExceptionHandler(SystemException.class)// 需要处理的异常类型
-    public Result doSystemException(SystemException ex){
-        // 记录日志
-        // 发送消息给运维
-        // 发送右键给开发人员
-
-        return new Result(ex.getCode(),null,ex.getMessage());
-    }
-
-    @ExceptionHandler(BusinessException.class)// 需要处理的异常类型
-    public Result doBusinessException(BusinessException ex){
-        // 记录日志
-        // 发送消息给运维
-        // 发送右键给开发人员，ex对象发送给开发人员
-
-        return new Result(ex.getCode(),null,ex.getMessage());
-    }
-
     @ExceptionHandler(Exception.class)// 需要处理的异常类型
     public Result doException(Exception ex){
-
-        // 记录日志
-        // 发送消息给运维
-        // 发送右键给开发人员，ex对象发送给开发人员
-        return new Result(Code.SYSTEM_UNKNOW_ERR,null,"系统繁忙，请稍后再试！");
+        System.out.println("异常"+ex);
+        return new Result(666,null,"出现异常"+ex);
     }
 }
