199d096403bf3274385f745f51e74decaa2b1a76
Whale <409134834@qq.com>
2024-07-18 16:21:32 +0800

    页面

--- src/main/java/com/whale/controller/BookController.java
+++ src/main/java/com/whale/controller/BookController.java
@@ -31,7 +31,6 @@ public class BookController {
     }
     @GetMapping("/{id}")
     public Result getById(@PathVariable Integer id) {
-
         Book book = bookService.getById(id);
         Integer code = book != null ? Code.GET_OK : Code.GET_ERR;
         String msg = book != null ? "" : "数据查询失败，请重试！";
