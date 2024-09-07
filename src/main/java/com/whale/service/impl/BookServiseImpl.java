d6e8142f12f9605113cf5dae8b016396bceeeee3
Whale <409134834@qq.com>
2024-09-07 15:23:18 +0800

    修复功能

--- src/main/java/com/whale/service/impl/BookServiseImpl.java
+++ src/main/java/com/whale/service/impl/BookServiseImpl.java
@@ -16,35 +16,32 @@ public class BookServiseImpl implements BookService {
     private BookDao bookDao;
     @Override
     public boolean save(Book book) {
-        bookDao.save(book);
-        return true;
+        return bookDao.save(book) > 0;
     }
 
     @Override
     public boolean update(Book book) {
-        bookDao.update(book);
-        return true;
+        return bookDao.update(book) > 0;
     }
 
     @Override
     public boolean delete(Integer id) {
-        bookDao.delete(id);
-        return true;
+        return bookDao.delete(id) > 0;
     }
 
     @Override
     public Book getById(Integer id) {
-        if(id == 1){
-            throw new BusinessException(Code.BUSINESS_ERR,"哈哈哈");
-        }
-        // 将可能出现的异常进行包装，转换成自定义的异常
-        try {
-            if(id == 2) {
-                int i = 1 / 0;
-            }
-        }catch (ArithmeticException e){
-            throw new SystemException(Code.SYSTEM_TIMEOUT_ERR,"服务器访问超时，请重试！",e);
-        }
+//        if(id == 1){
+//            throw new BusinessException(Code.BUSINESS_ERR,"哈哈哈");
+//        }
+//        // 将可能出现的异常进行包装，转换成自定义的异常
+//        try {
+//            if(id == 2) {
+//                int i = 1 / 0;
+//            }
+//        }catch (ArithmeticException e){
+//            throw new SystemException(Code.SYSTEM_TIMEOUT_ERR,"服务器访问超时，请重试！",e);
+//        }
 
 
         return bookDao.getById(id);
